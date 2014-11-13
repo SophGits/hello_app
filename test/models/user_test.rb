01fc3ed8ce7463fa0356f139ae4999fb158a5150
SophGits <sophie.jiffy@gmail.com>
2014-11-13 11:29:50 +0000

    Add name and email length validations.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,5 +17,15 @@ class UserTest < ActiveSupport::TestCase
   test "email should be present" do
      @user.email = "     "
      assert_not @user.valid?
-   end
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 256
+    assert_not @user.valid?
+  end
 end
